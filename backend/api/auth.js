256b677c1f9cbbc81ea865f98d438b8ba320f770
RafaDraw <www.rafadraw@gmail.com>
2018-12-17 16:29:00 -0200

    auth error

--- backend/api/auth.js
+++ backend/api/auth.js
@@ -13,7 +13,6 @@ module.exports = app => {
         if (!user) return res.status(400).send('Usuário não encontrado!');
 
         const isMatch = bcrypt.compareSync(req.body.password, user.password);
-
         if (!isMatch) return res.status(401).send('Email/Senha inválidos!');
 
         const now = Math.floor(Date.now() / 1000);
