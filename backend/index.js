fdc5b09031b23d55b6693f0d30cac4d4d1b6aa8a
RafaDraw <www.rafadraw@gmail.com>
2019-01-03 11:16:28 -0200

    backend finish

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,19 @@
 const app = require('express')();
 const consign = require('consign');
+const mongoose = require('mongoose');
 
 const db = require('./config/db');
+require('./config/mongodb');
 
 app.db = db;
+app.mongoose = mongoose;
 
 consign()
     .include('./config/passport.js')
     .then('./config/middlewares.js')
     .then('./api/validate.js')
     .then('./api')
+    .then('./schedule')
     .then('./config/routes.js')
     .into(app);
 
