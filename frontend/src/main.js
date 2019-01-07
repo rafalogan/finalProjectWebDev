2c91bdeb2cbaad96f323d34a2fddb33d2b406e8b
RafaDraw <www.rafadraw@gmail.com>
2019-01-07 16:13:25 -0200

    user post

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,10 +4,14 @@ import Vue from 'vue';
 import App from './App';
 
 import './config/bootstrap';
+import './config/msgs';
 import store from './config/store';
 import router from './config/router';
 
 Vue.config.productionTip = false;
+
+// TEMPORARIO!
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IlJhZmFlbCBDYW5kZWlyYSIsImVtYWlsIjoicm9vdEByb290LmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE1NDY4NzU1MjAsImV4cCI6MTU0NzEzNDcyMH0.zdclnn4pSS4XJHm_Wx_R-no3B2Al4lBObVz6Q916UWY';
 
 new Vue({
   store,
