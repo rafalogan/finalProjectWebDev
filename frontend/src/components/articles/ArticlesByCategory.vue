17b35f05a89bc38a6d0ec2e1181dc14a33ff911f
RafaDraw <www.rafadraw@gmail.com>
2019-01-09 17:07:29 -0200

    Article by Id and Category By articles

--- frontend/src/components/articles/ArticlesByCategory.vue
+++ frontend/src/components/articles/ArticlesByCategory.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="articles-by-category">
         <PageTitle icon="fa fa-folder-o" :main="category.name" sub="Categoria" />
+        <ul>
+            <li v-for="article in articles" :key="article.id">
+                {{ article.name }}
+            </li>
+        </ul>
+        <div class="load-more">
+            <button v-if="loadMore" class="btn btn-lg btn-outline-primary" @click="getArticles">
+                Carregar Mais Artigos
+            </button>
+        </div>
     </div>
 </template>
 
@@ -26,15 +36,34 @@
             getCategory() {
                 const url = `${baseApiUrl}/categories/${this.category.id}`;
                 axios(url).then(res => this.category = res.data)
+            },
+            getArticles() {
+                const url = `${baseApiUrl}/categories/${this.category.id}/articles?page=${this.page}`;
+                axios(url).then(res => {
+                    this.articles = this.articles.concat(res.data);
+                    this.page++;
+                    if (res.data.length === 0) this.loadMore = false;
+                })
             }
         },
         mounted() {
             this.category.id = this.$route.params.id;
-            this.getCategory()
+            this.getCategory();
+            this.getArticles()
         }
     }
 </script>
 
 <style scoped>
+    .articles-by-category ul {
+        list-style: none;
+        padding: 0px;
+    }
 
+    .articles-by-category .load-more {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 25px;
+    }
 </style>
