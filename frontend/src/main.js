c7d77d88ca2f8e5bd5c87d4006086799f2bd5ab9
RafaDraw <www.rafadraw@gmail.com>
2019-01-10 16:54:09 -0200

    Create Authentication

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,13 +5,11 @@ import App from './App';
 
 import './config/bootstrap';
 import './config/msgs';
+import './config/axios';
 import store from './config/store';
 import router from './config/router';
 
 Vue.config.productionTip = false;
-
-// TEMPORARIO!
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IlJhZmFlbCBDYW5kZWlyYSIsImVtYWlsIjoicm9vdEByb290LmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE1NDY4NzU1MjAsImV4cCI6MTU0NzEzNDcyMH0.zdclnn4pSS4XJHm_Wx_R-no3B2Al4lBObVz6Q916UWY';
 
 new Vue({
   store,
